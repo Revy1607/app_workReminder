5873338523ea0b3c043b11326b8fb6e6c26fa56e
Revy1607 <118365604+Revy1607@users.noreply.github.com>
2023-10-04 10:28:00 +0700

    Initial commit

---
+++ app/src/main/java/com/datnt/app_workreminder/Task.kt
@@ -0,0 +1,4 @@
+package com.datnt.app_workreminder
+
+class Task(var id: Int, var name: String, var details: String, var time: String ) {
+}
---
+++ app/src/main/java/com/datnt/app_workreminder/TaskAdapter.kt
@@ -0,0 +1,35 @@
+package com.datnt.app_workreminder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter (private var taskList: List<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private var tvTitle: TextView = view.findViewById(R.id.taskTitle)
+        private var tvDetails: TextView = view.findViewById(R.id.taskDetails)
+        private var tvDate: TextView = view.findViewById(R.id.tvDate)
+
+        fun setData(task: Task) {
+            tvTitle.text = task.name
+            tvDetails.text = task.details
+            tvDate.text = task.time
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = taskList.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.setData(taskList[position])
+
+    }
+}
